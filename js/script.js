49d664c110997e7792cf7953380e2410d01fd4bf
NubsOnly <lennert.vaes@outlook.com>
2021-11-22 22:31:29 +0100

    fixed accordion & made return statements index.html doesnt open accordion automatically

--- js/script.js
+++ js/script.js
@@ -19,8 +19,6 @@ function fetchQuality() {
               [i.center[0], i.center[1]] = [i.center[1], i.center[0]]
               lat = i.center[0];
               lon = i.center[1];
-              console.log("no length")
-              console.log(i.place_name.toUpperCase())
             }
           }
         }else {
@@ -29,42 +27,58 @@ function fetchQuality() {
                 [i.center[0], i.center[1]] = [i.center[1], i.center[0]]
                 lat = i.center[0];
                 lon = i.center[1];
-                console.log("length")
             }
         } 
         }
         fetch(`http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${lat}&lon=${lon}&appid=${key}`)
         .then(response => response.json()).then(data => {
-          let aqiValue = data.list[0].main.aqi
-          console.log(aqiValue)
-          if (aqiValue == 1){ 
-            return document.getElementById('apiTest').innerHTML = `<div class="alert alert-success" role="alert">
+          console.log(document.getElementById('collapseOne').classList)
+          let aqiValue = data.list[0].main.aqi;
+          let reset = document.querySelectorAll('#collapseOne, #collapseTwo, #collapseThree, #collapseFour, #collapseFive, #collapseSix');
+          for (i of reset){
+            i.classList.remove('show')
+          }
+          if (aqiValue == 1){
+            let accordionReturn = document.getElementById('collapseOne').classList.add('show') 
+            let apiReturn = document.getElementById('apiTest').innerHTML = `<div class="alert alert-success" role="alert">
             <iframe width="1900" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=${userInput}+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=31ad442e590a45bb42c3051d4d4e3370e63d5b73'></script>
           </div>`;
+          return [accordionReturn, apiReturn];
           }else if (aqiValue == 2){
-            return document.getElementById('apiTest').innerHTML = `<div class="alert alert-info" role="alert">
+            let accordionReturn = document.getElementById('collapseTwo').classList.add('show')
+            let apiReturn = document.getElementById('apiTest').innerHTML = `<div class="alert alert-info" role="alert">
             <iframe width="1900" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=${userInput}+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=31ad442e590a45bb42c3051d4d4e3370e63d5b73'></script>
           </div>`;
+          return [accordionReturn, apiReturn];
   
           }else if (aqiValue == 3){
-            return document.getElementById('apiTest').innerHTML = `<div class="alert alert-warning" role="alert">
+            let accordionReturn = document.getElementById('collapseThree').classList.add('show')
+            let apiReturn = document.getElementById('apiTest').innerHTML = `<div class="alert alert-warning" role="alert">
             <iframe width="1900" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=${userInput}+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=31ad442e590a45bb42c3051d4d4e3370e63d5b73'></script>
           </div>`;
+          return [accordionReturn, apiReturn];
   
           }else if (aqiValue == 4){
-            return document.getElementById('apiTest').innerHTML = `<div class="alert alert-dark" role="alert">
+            let accordionReturn = document.getElementById('collapseFour').classList.add('show')
+            let apiReturn = document.getElementById('apiTest').innerHTML = `<div class="alert alert-dark" role="alert">
             <iframe width="1900" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=${userInput}+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=31ad442e590a45bb42c3051d4d4e3370e63d5b73'></script>
           </div>`;
+          return [accordionReturn, apiReturn];
   
           }else if (aqiValue == 5){
-            return document.getElementById('apiTest').innerHTML = `<div class="alert alert-secondary" role="alert">
+            let accordionReturn = document.getElementById('collapseFive').classList.add('show')
+            let apiReturn = document.getElementById('apiTest').innerHTML = `<div class="alert alert-secondary" role="alert">
             <iframe width="1900" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=${userInput}+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=31ad442e590a45bb42c3051d4d4e3370e63d5b73'></script>
           </div>`;
+          return [accordionReturn, apiReturn];
   
           }else if (aqiValue == 6){
-            return document.getElementById('apiTest').innerHTML = `<div class="alert alert-danger" role="alert">
+            let accordionReturn = document.getElementById('collapseSix').classList.add('show')
+            let apiReturn = document.getElementById('apiTest').innerHTML = `<div class="alert alert-danger" role="alert">
             <iframe width="1900" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=${userInput}+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=31ad442e590a45bb42c3051d4d4e3370e63d5b73'></script>
           </div>`;
+          return [accordionReturn, apiReturn];
+          
           }
         })
         
